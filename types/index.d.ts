bb1893c3d3c799de422b588aa7e330c4b54230e6
刘小呆呆呀 <daidaibg@163.com>
2022-02-21 17:38:23 +0800

    增加drag声明文件

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,11 +1,6 @@
-/*
- * @Author: daidai
- * @Date: 2021-09-15 15:45:05
- * @LastEditors: daidai
- * @LastEditTime: 2021-09-29 10:08:52
- * @FilePath: \yhht-ui\yhht-ui\types\index.d.ts
- */
+
+
 export * from './yhht-ui'
 
-import * as YHHTUI from './yhht-ui'
-export default YHHTUI
+import * as YhhtUI from './yhht-ui'
+export default YhhtUI
